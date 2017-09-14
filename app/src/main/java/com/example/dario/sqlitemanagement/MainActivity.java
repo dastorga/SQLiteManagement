182a09492c3b4f4d3d38458d11b5812a36196126
Dario <astorgadm@gmail.com>
2017-09-14 18:13:18 -0300

    .
    .

--- app/src/main/java/com/example/dario/sqlitemanagement/MainActivity.java
+++ app/src/main/java/com/example/dario/sqlitemanagement/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.dario.sqlitemanagement;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,52 +15,82 @@ public class MainActivity extends AppCompatActivity {
         MiBaseDatos MDB = new MiBaseDatos(getApplicationContext());
 
 
-        // Escribimos 8 registros en nuestra tabla
-        MDB.insertUser("Pedro", 111111111, "pedro@DB.es");
-        MDB.insertUser("Sandra", 222222222, "sandra@DB.es");
-        MDB.insertUser("Maria", 333333333, "maria@DB.es");
-        MDB.insertUser("Daniel", 444444444, "daniel@DB.es");
-        MDB.insertUser("Dario", 444444444, "daniel@DB.es");
-        MDB.insertUser("Manuel", 444444444, "daniel@DB.es");
-        MDB.insertUser("Jose", 444444444, "daniel@DB.es");
-        MDB.insertUser("Mario", 444444444, "daniel@DB.es");
+
+//        MDB.insertUser("Pedro", 111111111, "pedro@DB.es");
+//        MDB.insertUser("Sandra", 222222222, "sandra@DB.es");
+//        MDB.insertUser("Maria", 333333333, "maria@DB.es");
+//        MDB.insertUser("Daniel", 444444444, "daniel@DB.es");
+//        MDB.insertUser("Dario", 444444444, "daniel@DB.es");
+//        MDB.insertUser("Manuel", 444444444, "daniel@DB.es");
+//        MDB.insertUser("Jose", 444444444, "daniel@DB.es");
+//        MDB.insertUser("Mario", 444444444, "daniel@DB.es");
+//
+//        MDB.insertUser("Pedro", 111111111, "pedro@DB.es");
+//        MDB.insertUser("Sandra", 222222222, "sandra@DB.es");
+//        MDB.insertUser("Maria", 333333333, "maria@DB.es");
+//        MDB.insertUser("Daniel", 444444444, "daniel@DB.es");
+//        MDB.insertUser("Dario", 444444444, "daniel@DB.es");
+//        MDB.insertUser("Manuel", 444444444, "daniel@DB.es");
+//        MDB.insertUser("Jose", 444444444, "daniel@DB.es");
+//        MDB.insertUser("Mario", 444444444, "daniel@DB.es");
+//
+//        MDB.insertUser("9", 111111111, "pedro@DB.es");
+//        MDB.insertUser("10", 222222222, "sandra@DB.es");
+//        MDB.insertUser("11", 333333333, "maria@DB.es");
+//        MDB.insertUser("12", 444444444, "daniel@DB.es");
+//        MDB.insertUser("13", 444444444, "daniel@DB.es");
+//        MDB.insertUser("14", 444444444, "daniel@DB.es");
+//        MDB.insertUser("15", 444444444, "daniel@DB.es");
+//        MDB.insertUser("16", 444444444, "daniel@DB.es");
+
+
 
 
 
 
         // Recuperamos los 4 registros y los mostramos en el log
-        Log.d("TOTAL", Integer.toString(MDB.recoverUsers().size()));
+//        Log.d("TOTAL", Integer.toString(MDB.recoverUsers().size()));
+//
+//        int[] ids = new int[MDB.recoverUsers().size()];
+//
+//        String[] names = new String[MDB.recoverUsers().size()];
+//
+//        int[] phones = new int[MDB.recoverUsers().size()];
+//
+//        String[] emails = new String[MDB.recoverUsers().size()];
+//
+//
+//        for (int i = 0; i < MDB.recoverUsers().size(); i++) {
+//
+//            ids[i] = MDB.recoverUsers().get(i).getId();
+//
+//            names[i] = MDB.recoverUsers().get(i).getName();
+//
+//            phones[i] = MDB.recoverUsers().get(i).getPhone();
+//
+//            emails[i] = MDB.recoverUsers().get(i).getEmail();
+//
+//            Log.d(""+ids[i], names[i] + ", " + phones[i] + ", " + emails[i]);
+//        }
 
-        int[] ids = new int[MDB.recoverUsers().size()];
-
-        String[] names = new String[MDB.recoverUsers().size()];
-
-        int[] phones = new int[MDB.recoverUsers().size()];
-
-        String[] emails = new String[MDB.recoverUsers().size()];
 
 
         for (int i = 0; i < MDB.recoverUsers().size(); i++) {
 
-            ids[i] = MDB.recoverUsers().get(i).getId();
+            String name = MDB.recoverUsers().get(i).getName();
+            Toast.makeText(getApplicationContext(), name , Toast.LENGTH_SHORT).show();
 
-            names[i] = MDB.recoverUsers().get(i).getName();
-
-            phones[i] = MDB.recoverUsers().get(i).getPhone();
-
-            emails[i] = MDB.recoverUsers().get(i).getEmail();
-
-            Log.d(""+ids[i], names[i] + ", " + phones[i] + ", " + emails[i]);
         }
 
 
 
-        for (int i = 0; i < MDB.recoverUsers().size(); i++) {
-            MDB.deleteUser(i);
-        }
 
 
-        Log.d("Total de USERS:", String.valueOf(MDB.recoverUsers().size()));
+
+
+
+
+//        Log.d("Total de USERS", String.valueOf(MDB.recoverUsers().size()));
 
 
 //        // Modificamos el registro 3
